4e1d4664532e63025723187da466cfb75e12d58b
Tak <oit5538@gmail.com>
2023-05-25 16:47:21 +0900

    feat: Create PhotoListEndpoint

---
+++ Unsplash_MVVMClean/Data/Endpoint/PhotoListEndpoint.swift
@@ -0,0 +1,22 @@
+//
+//  PhotoListEndpoint.swift
+//  Unsplash_MVVMClean
+//
+//  Created by Tak on 2023/05/20.
+//
+
+import Foundation
+
+struct PhotoListEndpoint: UnsplashAPIEndpoint {
+    typealias APIResponse = PhotoResponse
+    
+    var pageNum: Int
+    var path: String = "/photos"
+    var queries: [String : String] {
+        return [
+            "page": "\(pageNum)",
+            "per_page": "30",
+            "client_id": "\(serviceKey)"
+        ]
+    }
+}
