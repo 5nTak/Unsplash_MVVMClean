4708966fc27a68bd7870156f3d229f469ed628cd
Tak <oit5538@gmail.com>
2023-05-26 21:20:03 +0900

    feat: Create CategoryUseCase

---
+++ Unsplash_MVVMClean/Domain/UseCase/CategoryUseCase.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryUseCase.swift
+//  Unsplash_MVVMClean
+//
+//  Created by Tak on 2023/05/26.
+//
+
+import Foundation
+
+final class CategoryUseCase {
+    private let categoryRepository: CategoryRepository
+    
+    init(categoryRepository: CategoryRepository) {
+        self.categoryRepository = categoryRepository
+    }
+    
+    func fetchCategories(completion: @escaping (Result<[Category], Error>) -> Void) -> URLSessionTask? {
+        return categoryRepository.fetchCategories(completion: completion)
+    }
+}
