f1b0f57d92626443339d79255a6a1484931259a7
Tak <oit5538@gmail.com>
2023-06-13 19:29:04 +0900

    feat: Create PhotoListCollectionViewDataSource

---
+++ Unsplash_MVVMClean/Presentation/PhotoListView/PhotoListCollectionViewDataSource.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoListCollectionViewDataSource.swift
+//  Unsplash_MVVMClean
+//
+//  Created by Tak on 2023/06/01.
+//
+
+import UIKit
+
+final class PhotoListCollectionViewDataSource: NSObject, UICollectionViewDataSource {
+    var photos: [Photo] = []
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoListCollectionViewCell.identifier, for: indexPath) as? PhotoListCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.setup(photo: photos[indexPath.row])
+        
+        return cell
+    }
+}
