b9cdb0d713014b541e84c71d3f7685676b89453b
Tak <oit5538@gmail.com>
2023-06-21 19:56:25 +0900

    feat: Create LoginNavigationController

---
+++ Unsplash_MVVMClean/Presentation/LoginView/LoginNavigationController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginNavigationController.swift
+//  Unsplash_MVVMClean
+//
+//  Created by Tak on 2023/06/16.
+//
+
+import UIKit
+
+final class LoginNavigationController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
+}
